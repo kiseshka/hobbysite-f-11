82b28d06fa8c8934eed9052d12fcb8f4e2ff2d93
jamesmacasaet <james.emmanuelle.m@gmail.com>
2024-04-25 21:33:59 +0800

    fixed template design

--- hobbysite/wiki/admin.py
+++ hobbysite/wiki/admin.py
@@ -4,22 +4,20 @@ from .models import Article, ArticleCategory
 class ArticleCategoryAdmin(admin.ModelAdmin):
     model = ArticleCategory
 
-    search_fields=['name']
-    list_display=['name']
+    search_fields=["name"]
+    list_display=["name"]
 
-    fieldsets =[("Details", {"fields:" ("name", "description")})]
+    fieldsets = [("Details", {"fields": ("name", "description")})]
+
 
 class ArticleAdmin(admin.ModelAdmin):
     model = Article
 
-    search_fields = [
-        "title",
-    ]
+    search_fields = ["title"]
     list_display = ["title", "category", "created_on", "updated_on"]
     list_filter = ["created_on", "updated_on",]
 
-    fieldsets =[("Details", {"fields:" [("title", "entry"), "category"})]
+    fieldsets =[("Details", {"fields": [("title", "entry"), "category"]})]
 
 admin.site.register(Article, ArticleAdmin)
 admin.site.register(ArticleCategory, ArticleCategoryAdmin)
-
